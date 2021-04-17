e8453af4d797f579f218139150246beda184d37d
Bivash Thakuri <tbivash3@gmail.com>
2021-04-16 23:21:29 -0500

    Init project with random lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SortingVisualizer';
+
+  constructor() {}
+
+  @Input()
+  randomNumberList: number[] = [];
+
+  ngOnInit() {
+    this.generateRandomNumberList();
+  }
+
+  generateRandomNumberList() {
+    this.randomNumberList = Array.from({ length: 200 }, () =>
+      Math.floor(Math.random() * 400)
+    );
+  }
+
+  sort() {
+    this.generateRandomNumberList();
+  }
 }
