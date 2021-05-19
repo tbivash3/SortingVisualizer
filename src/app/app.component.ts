e7ed2c29274d989be6a410f4a9a057b3c62bb57f
Bivash Thakuri <tbivash3@gmail.com>
2021-05-18 22:07:14 -0500

    update mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { HeapSort } from './sortingAlgorithms/HeapSort';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements AfterViewInit {
   title = 'SortingVisualizer';
 
   constructor(
